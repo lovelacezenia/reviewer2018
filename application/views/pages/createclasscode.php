3806386f6a25e8d2df8c6a351a456108d2f16cbd
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-04-27 23:21:43 +0800

    added profile ui in admin

--- application/views/pages/createclasscode.php
+++ application/views/pages/createclasscode.php
@@ -10,38 +10,38 @@
                                         <div class="form-group row">
                                             <label class="col-lg-4 col-form-label">Classcode</label>
                                             <div class="col-lg-6">
-                                                <input type="text" class="form-control" id="classcode" name="classcode" placeholder="Enter classcode">
+                                                <input type="text" class="form-control" name="classcode" placeholder="Enter classcode">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-lg-4 col-form-label">Course no.</label>
                                             <div class="col-lg-6">
-                                                <input type="text" class="form-control" id="course_no" min="0" name="course_no" placeholder="Enter course no">
+                                                <input type="text" class="form-control" min="0" name="course_no" placeholder="Enter course no">
                                             </div>
                                         </div>
                                         
                                         <div class="form-group row">
                                             <label class="col-lg-4 col-form-label">Description Title</label>
                                             <div class="col-lg-6">
-                                                <textarea class="form-control" id="description" name="description" placeholder="Insert Description title"></textarea>
+                                                <textarea class="form-control" name="description" placeholder="Insert Description title"></textarea>
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-lg-4 col-form-label">Units</label>
                                             <div class="col-lg-6">
-                                                <input type="text" class="form-control" id="units" name="units" placeholder="Enter units">
+                                                <input type="text" class="form-control" name="units" placeholder="Enter units">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-lg-4 col-form-label">Time and days</label>
                                             <div class="col-lg-6">
-                                                <input type="text" class="form-control" id="time_and_days" name="time_and_days" placeholder="Enter time and days..">
+                                                <input type="text" class="form-control" name="time_and_days" placeholder="Enter time and days..">
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label class="col-lg-4 col-form-label">Room</label>
                                             <div class="col-lg-6">
-                                                <input type="text" class="form-control" id="room" name="room" placeholder="Enter room">
+                                                <input type="text" class="form-control" name="room" placeholder="Enter room">
                                             </div>
                                         </div>
                                         <div class="form-group row">
