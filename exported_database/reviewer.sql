22725250a4107b531554c1c5a3a643be76002e55
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-04-27 17:59:49 +0800

    update database

--- exported_database/reviewer.sql
+++ exported_database/reviewer.sql
@@ -1,86 +1,84 @@
-CREATE DATABASE  IF NOT EXISTS `reviewer` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `reviewer`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: reviewer
--- ------------------------------------------------------
--- Server version	5.7.19
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2018 at 11:58 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reviewer`
+--
+CREATE DATABASE IF NOT EXISTS `reviewer` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `reviewer`;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `classcode`
 --
 
-DROP TABLE IF EXISTS `classcode`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `classcode` (
-  `class_id` int(15) NOT NULL AUTO_INCREMENT,
+  `class_id` int(15) NOT NULL,
   `classcode` varchar(250) NOT NULL,
   `description` varchar(250) NOT NULL,
   `room` varchar(20) NOT NULL,
   `time_and_days` varchar(250) NOT NULL,
   `title` varchar(250) NOT NULL,
   `units` int(4) NOT NULL,
-  PRIMARY KEY (`class_id`),
-  UNIQUE KEY `class_id` (`class_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `classcode`
 --
 
-LOCK TABLES `classcode` WRITE;
-/*!40000 ALTER TABLE `classcode` DISABLE KEYS */;
-INSERT INTO `classcode` VALUES (1,'9345B','Web System and Technologies laboratory','D323','4:00-5:30 THS','IT 325',1),(2,'9364A','Web System and Technologies','D515','1:00-2:00TF','IT 324',2);
-/*!40000 ALTER TABLE `classcode` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `classcode` (`class_id`, `classcode`, `description`, `room`, `time_and_days`, `title`, `units`, `timestamp`) VALUES
+(1, '9345B', 'Web System and Technologies laboratory', 'D323', '4:00-5:30 THS', 'IT 325', 1, '2018-04-27 08:08:26'),
+(2, '9364A', 'Web System and Technologies', 'D515', '1:00-2:00TF', 'IT 324', 2, '2018-04-27 08:08:26');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `scores`
 --
 
-DROP TABLE IF EXISTS `scores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `scores` (
-  `score_id` int(11) NOT NULL AUTO_INCREMENT,
+  `score_id` int(11) NOT NULL,
   `score` int(250) NOT NULL,
   `quiz_date` varchar(250) NOT NULL,
   `total_score` int(250) NOT NULL,
-  PRIMARY KEY (`score_id`)
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `scores`
 --
 
-LOCK TABLES `scores` WRITE;
-/*!40000 ALTER TABLE `scores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `scores` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `scores` (`score_id`, `score`, `quiz_date`, `total_score`, `timestamp`, `user_id`) VALUES
+(1, 20, 'april 25, 2018', 50, '2018-04-27 08:23:36', 0);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `user_id` int(250) NOT NULL AUTO_INCREMENT,
+  `user_id` int(250) NOT NULL,
   `firstname` varchar(250) NOT NULL,
   `lastname` varchar(250) NOT NULL,
   `idno` int(6) NOT NULL,
@@ -90,31 +88,66 @@ CREATE TABLE `user` (
   `timestamp` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `position` varchar(250) NOT NULL,
   `Status` varchar(250) NOT NULL DEFAULT 'Active',
-  `class_id` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id` (`user_id`),
-  UNIQUE KEY `idno` (`idno`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `class_id` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Lovelace','Oliva',2144074,'094868745151','1234','oliva@gmail.com','2018-04-23 07:26:27.166411','Admin','Active','2'),(3,'Sarah','Centino',2152834,'09057747156','12345','2152834@slu.edu.ph','2018-04-23 10:27:24.409749','Student','Active','2');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `idno`, `contact_no`, `password`, `email`, `timestamp`, `position`, `Status`, `class_id`) VALUES
+(1, 'Lovelace', 'Oliva', 2144074, '094868745151', '1234', 'oliva@gmail.com', '2018-04-23 07:26:27.166411', 'Admin', 'Active', '2'),
+(3, 'Sarah', 'Centino', 2152834, '09057747156', '12345', '2152834@slu.edu.ph', '2018-04-23 10:27:24.409749', 'Student', 'Active', '2');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `classcode`
+--
+ALTER TABLE `classcode`
+  ADD PRIMARY KEY (`class_id`),
+  ADD UNIQUE KEY `class_id` (`class_id`);
+
+--
+-- Indexes for table `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`score_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `idno` (`idno`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `classcode`
+--
+ALTER TABLE `classcode`
+  MODIFY `class_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `score_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-04-26 18:56:53
