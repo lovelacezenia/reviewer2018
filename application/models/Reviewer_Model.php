b475ae069864f9ba822da42f000e4e45e270886d
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-04-27 20:52:35 +0800

    update ui in admin

--- application/models/Reviewer_Model.php
+++ application/models/Reviewer_Model.php
@@ -12,6 +12,14 @@ class Reviewer_Model extends CI_Model
 
     	return $this->db->select('concat(firstname, " " ,lastname) as name, scores.*, idno')
     				->join('user','user.user_id = score_id')
-    				->get('scores')->result_array();
+    				->get('scores')
+                    ->result_array();
+    }
+
+    public function getEnrolledstudent(){
+        return $this->db->select('concat(firstname, " ", lastname) as name, idno')
+                    ->join('user', 'user.user_id = classcode.class_id')
+                    ->get('classcode')
+                    ->result_array();
     }
 }
