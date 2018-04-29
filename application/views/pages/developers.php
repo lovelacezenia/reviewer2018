635b3144c66cbb757c161714c0acbae04e06a7a5
DianneCayabyab <diannecayabyab@gmail.com>
2018-04-29 21:22:40 +0800

    Create developers.php

---
+++ application/views/pages/developers.php
@@ -0,0 +1,71 @@
+<div id="colorlib-testimony" class="colorlib-light-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center animate-box intro-heading">
+						<h2>Our Team</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="row animate-box">
+							<div class="owl-carousel1">
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="assets/images/dianne.png" alt="user">
+										</figure>
+										<blockquote>
+											<p>Dianne Alyza M. Cayabyab</p>
+											<span>Front-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="assets/images/sarah.jpg" alt="user">
+										</figure>
+										<blockquote>
+											<p> Sarah Mae E. Centino</p>
+											<span>Front-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="assets/images/person2.jpg" alt="user">
+										</figure>
+										<blockquote>
+											<p>Graham Ross H. Flora</p>
+											<span>Back-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="assets/images/person3.jpg" alt="user">
+										</figure>
+										<blockquote>
+											<p>Lovelace Zennia Luisa R. Oliva</p>
+											<span>Back-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="assets/images/person3.jpg" alt="user">
+										</figure>
+										<blockquote>
+											<p>John Yves B. Ragsac</p>
+											<span></span>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
