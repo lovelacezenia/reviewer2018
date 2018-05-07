8fe0ef9438e5ab46407a1cfe612aeb1acaaae7fb
GrahamFlora <grahamrossflora@gmail.com>
2018-05-07 20:35:30 +0800

    update addrow in quiz

--- application/views/pages/createquiz.php
+++ application/views/pages/createquiz.php
@@ -14,52 +14,50 @@ if(!isset($this->session->userdata['logged_in'])){
                                 <!-- Nav tabs -->
                                 <ul id="tab" class="nav nav-tabs" role="tablist">
                                     <li class="nav-item">
-                                        <a class="nav-link active" data-toggle="tab" href="#" role="tab">
+                                        <a class="nav-link" data-toggle="tab" href="#" role="tab">
                                             <span class="hidden-sm-up"><i class="ti-home"></i></span>
-                                            <span class="hidden-xs-down">Section </span>
+                                            <span class="hidden-xs-down">Section 1</span>
                                         </a>
                                     </li>
                                 </ul>
-                                <button class="btn btn-success hidden-xs-down" onclick="addrow();">+</button>
-                                <!-- Tab panes -->
-                                    <div id="tab1" role="sec">
-                                        <div class="p-20">
-                                        <div class="col-lg-7">
-
-                                            <h4 class="card-title">type of quiz </h4>
-                                            <div class="card-content">
-                                                <div class="todo-list">
-                                                    <div class="tdl-holder">
-                                                        <div class="tdl-content">
-                                                            <ul>
-                                                                <li>
-                                                                    <label>
-                                                                        <input type="checkbox"><i class="bg-primary"></i><span>Build an angular app</span>
-                                                                        <a href='#'></a>
-                                                                    </label>
-                                                                </li>
-                                                                <li>
-                                                                    <label>
-                                                                        <input type="checkbox"><i class="bg-success"></i><span>Creating component page</span>
-                                                                        <a href='#'></a>
-                                                                    </label>
-                                                                </li>
-                                                            </ul>
-                                                        </div>
-                                                        <input id="add" type="text" class="tdl-new form-control" placeholder="Type here">
-                                                        <button type="submit" class="btn btn-primary" onclick="addbutton();">Add</button>
+                                <button class="btn btn-primary hidden-xs-down" onclick="addrow();">+</button>
+                        <!-- Tab panes -->
+                             <div id="tab1" role="sec">
+                                 <div class="p-20">
+                                      <div class="col-lg-7">
+                                           <h4 class="card-title">type of quiz </h4>
+                                            <div class="row p-t-30" id="input">
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <select name="inputs" id="inputs" class="form-control">
+                                                            <option value="1">Multiple Choices</option>
+                                                            <option value="2">True or False</option>
+                                                            <option value="3">Indentification</option>
+                                                        </select>
                                                     </div>
                                                 </div>
+                                                <div class="col-md-3">
+                                                    <div class="form-group">
+                                                        <input id="numberOfQuestions" type="text" class="tdl-new form-control" min="0" placeholder="Typeno">
+                                                    </div>
+                                                </div>
+
+                                            <div class="col-md-0">
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary" onclick="addbutton();">go</button>
+                                                </div>
                                             </div>
-                                        </div>
+                                       </div>
                                     </div>
+                                        </div>
+                                 <div id="inputQuestions">
+
+                                 </div>
                                     <div class="form-group row">
-                                            <div class="col-lg-8 ml-auto">
-                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                            <div class="col-lg-2 ml-auto">
+                                                <button type="submit" class="btn btn-primary">Save</button>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="tab-pane  p-20" id="sec1" role="sec">  </div>
+                                     </div>
                             </div>
                         </div>
                     </div>
