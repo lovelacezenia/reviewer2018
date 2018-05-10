4fa1f71eedae7fea08151a10ae283e9bb2534d53
Sarah Mae Centino <2152834@slu.edu.ph>
2018-05-10 18:10:11 +0800

    updated home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -11,7 +11,7 @@
 							<span class="icon"><i class="icon-pen3"></i></span>
 							<div class="desc">
 								<h3>Web Design</h3>
-								<p>description</p>
+								<p>The creation of looks, layout and features of a website.</p>
 							</div>
 						</div>
 					</div>
@@ -20,7 +20,7 @@
 							<span class="icon"><i class="icon-diamond"></i></span>
 							<div class="desc">
 								<h3>Web Development</h3>
-								<p>description</p>
+								<p>Developing a website in the internet.</p>
 							</div>
 						</div>
 					</div>
@@ -29,7 +29,7 @@
 							<span class="icon"><i class="icon-paperplane"></i></span>
 							<div class="desc">
 								<h3>Java Servlets</h3>
-								<p>description</p>
+								<p>A java program that extends the capabilities of a server.</p>
 							</div>
 						</div>
 					</div>
@@ -40,7 +40,7 @@
 							<span class="icon"><i class="icon-params"></i></span>
 							<div class="desc">
 								<h3>Node JS</h3>
-								<p>description</p>
+								<p>An open-source, cross-platform JavaScript run-time environment that executes JavaScript code server-side.</p>
 							</div>
 						</div>
 					</div>
@@ -49,7 +49,7 @@
 							<span class="icon"><i class="icon-photo"></i></span>
 							<div class="desc">
 								<h3>Session Handling</h3>
-								<p>description</p>
+								<p>A way to store information (in variables) to be used across multiple pages.</p>
 							</div>
 						</div>
 					</div>
@@ -93,35 +93,6 @@
 										<p class="read-more">Read more <i class="icon-arrow-right3"></i></p>
 									</a>
 								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div id="colorlib-counter" class="colorlib-counters">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2 text-center animate-box intro-heading">
-						<h2>Fun Facts</h2>
-						<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-					</div>
-				</div>
-				<div class="row animate-box">
-					<div class="col-md-10 col-md-offset-1">
-						<div class="row">
-							<div class="col-md-4 text-center">
-								<span class="colorlib-counter js-counter" data-from="0" data-to="456" data-speed="5000" data-refresh-interval="50"></span>
-								<span class="colorlib-counter-label">Coffee</span>
-							</div>
-							<div class="col-md-4 text-center">
-								<span class="colorlib-counter js-counter" data-from="0" data-to="899" data-speed="5000" data-refresh-interval="50"></span>
-								<span class="colorlib-counter-label">Done Projects</span>
-							</div>
-							<div class="col-md-4 text-center">
-								<span class="colorlib-counter js-counter" data-from="0" data-to="2000" data-speed="5000" data-refresh-interval="50"></span>
-								<span class="colorlib-counter-label">Subscribers</span>
 							</div>
 						</div>
 					</div>
