9ac90b46e98308ad70c7380d390f8d1f9a1f5272
DianneCayabyab <diannecayabyab@gmail.com>
2018-05-12 01:33:23 +0800

    Update ajax.php

--- application/views/pages/ajax.php
+++ application/views/pages/ajax.php
@@ -9,39 +9,7 @@
 	<meta name="keywords" content="" />
 	<meta name="author" content="" />
 
-  <!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
-
-	<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,700" rel="stylesheet">
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-	<link rel="stylesheet" href="css/style.css">
-
-
+ 
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
 	<!-- FOR IE9 below -->
@@ -68,19 +36,7 @@
 		</div>
 	</nav>
 	
-	<div id="colorlib-page">
-		<header>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="colorlib-navbar-brand">
-							<a class="colorlib-logo" href="index.html">Suitcase</a>
-						</div>
-						<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-					</div>
-				</div>
-			</div>
-		</header>
+	
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
@@ -102,111 +58,30 @@
 		<div id="colorlib-work">
 			<div class="container">
 				<div class="row row-padded-bottom">
-					<div class="col-md-7 animate-box">
-						<img class="img-responsive" src="images/work-1.jpg" alt="work">
-						<img class="img-responsive" src="images/work-2.jpg" alt="work">
-						<img class="img-responsive" src="images/work-3.jpg" alt="work">
-						<img class="img-responsive" src="images/work-4.jpg" alt="work">
+					<div class="col-md-6 animate-box">
+						<h1>Asynchronous JavaScript and XML</h1>
+						<p>-It allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page.</p>
+						<h2>AJAX uses a combination of:</h2>
+						<p>-A browser built-in XMLHttpRequestobject(to tequest data from a web server).</p>
+						<p>-JavaScript and HTML DOM(to display or use the data)</p>
 					</div>
-					<div class="col-md-4 col-md-push-1 animate-box">
+					<div class="col-md-6 col-md-push-1 animate-box">
 						<div class="work-detail">
-							<p class="tag"><span>Illustration</span>, <span>Logo</span></p>
-							<h2>Work 01</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-							<p><a href="#" class="btn btn-primary">View Live Preview</a></p>
+							<h1> How AJAX Works: </h1>
+							<p>1. An event occurs in a web page</p>
+							<p>2. An XMLHttpRequest object is created by JavaScript</p>
+							<p>3. The XMLHttpRequest object sends a request to a web server</p>
+							<p>4. The server processes the request</p>
+							<p>5. The server sends a response back to the web page</p>
+							<p>6. The response is read by JavaScript</p>
+							<p>7. Proper action (like page update) is performed by JavaScript</p>
+							
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<h2>Related Works</h2>
-					</div>
-					<div class="col-md-6 animate-box text-center">
-						<a href="work-single.html" class="work-img" style="background-image: url(images/work-1.jpg);">
-							<div class="overlay"></div>
-							<div class="desc">
-								<span class="icon"><i class="icon-heart-outline"></i></span>
-								<p class="tag"><span>Illustration</span>, <span>Logo</span></p>
-								<h3>Work 01</h3>
-								<span class="read-more">Read more <i class="icon-arrow-right3"></i></span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-6 animate-box text-center">
-						<a href="work-single.html" class="work-img" style="background-image: url(images/work-2.jpg);">
-							<div class="overlay"></div>
-							<div class="desc">
-								<span class="icon"><i class="icon-heart-outline"></i></span>
-								<p class="tag"><span>Web Design</span>, <span>UI</span></p>
-								<h3>Work 02</h3>
-								<span class="read-more">Read more <i class="icon-arrow-right3"></i></span>
-							</div>
-						</a>
-					</div>
-				</div>
+				
 			</div>
-		</div>
-
-		<div id="colorlib-hire" class="colorlib-light-grey">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-md-offset-1 col-md-pull-1 animate-box">
-						<h2 class="heading">Are you looking for a web designer?</h2>
-						<p><a href="#" class="btn btn-primary btn-lg">Available for Hire!</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<footer>
-			<div id="footer">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4 col-pb-sm">
-							<div class="row">
-								<div class="col-md-10">
-									<h2>Suitcase</h2>
-									<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-									<div class="row">
-										<div class="col-md-6">
-											<p>From the countries Vokalia and Consonantia</p>
-										</div>
-										<div class="col-md-6">
-											<p>Far far away, behind the word mountains</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 col-pb-sm">
-							<h2>Newsletter</h2>
-							<p>A small river named Duden flows by their place and supplies it with the necessary regelialia</p>
-							<div class="subscribe text-center">
-								<div class="form-group">
-									<input type="text" class="form-control text-center" placeholder="Enter Email address">
-									<input type="submit" value="Subscribe" class="btn btn-primary btn-custom">
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 col-pb-sm right-display">
-							<h2>Follow Us</h2>
-							<p class="colorlib-social-icons colorlib-social-icons2">
-								<a href="#"><i class="icon-facebook4"></i></a>
-								<a href="#"><i class="icon-twitter3"></i></a>
-								<a href="#"><i class="icon-googleplus"></i></a>
-								<a href="#"><i class="icon-dribbble2"></i></a>
-							</p>
-							<p>
-								<span class="block"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart2" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --><br></span> 
-								<span class="block">Demo Images: <a href="http://nothingtochance.co/" target="_blank">nothingtochance.co</a> <a href="http://unsplash.co/" target="_blank">Unsplash</a></span>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
+		
 	
 	</div>
 
