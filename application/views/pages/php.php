e54c8fcb5af15db02ebc75f3be8dc9fba9cf8375
DianneCayabyab <diannecayabyab@gmail.com>
2018-05-12 00:54:16 +0800

    Create php.php

---
+++ application/views/pages/php.php
@@ -0,0 +1,129 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Suitcase Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+ 
+
+	
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<nav id="colorlib-main-nav" role="navigation">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
+		<div class="js-fullheight colorlib-table">
+			<div class="colorlib-table-cell js-fullheight">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="work.html">Work</a></li>
+					<li><a href="services.html">Services</a></li>
+					<li><a href="blog.html">Case Studies</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/cover_img_4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container">
+			   			<div class="row">
+				   			<div class="col-md-8 col-sm-12 col-xs-12 col-md-offset-2 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>PHP</h1>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="colorlib-work">
+			<div class="container">
+				<div class="row row-padded-bottom">
+					<div class="col-md-6 animate-box">
+					<h1>Personal Home Page(PHP)</h1>
+					<p>-is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML.</p>
+
+					<h1>PHP SuperGlobals variables:</h1>
+					<h2>1. $GLOBALS</h2>
+					<p>-is used to access global variables from anywhere in the PHP script (also from within functions or methods).</p> 
+					<h2>2. $_SERVER</h2>
+					<p>-is a PHP super global variable which holds information about headers, paths, and script locations.</p>
+				    <h2>3. $_REQUEST</h2>
+				    <p>-is used to collect data after submitting an HTML form.</p>
+				    <h2>4. $_POST</h2>
+				    <p>-is widely used to collect form data after submitting an HTML form with method="post". $_POST is also widely used to pass variables.</p>
+				    <h2>5. $_GET</h2>
+				    <p>-can also be used to collect form data after submitting an HTML form with method="get". $_GET can also collect data sent in the URL.</p>
+				   
+	
+					</div>
+					<div class="col-md-6 col-md-push-1 animate-box">
+						<div class="work-detail">
+						<h2>6. $_FILES</h2>
+				   	<h2>7. $_ENV</h2>
+				    <h2>8. $_COOKIE</h2>
+				    <h2>9. $_SESSION</h2>
+						<h1>Database Extensions</h1>
+						<h2>Abstraction Layer:</h2>
+						<h3>o	DBA — Database ( dbm -style) Abstraction Layer</h3>
+						<h3>o	dbx</h3>
+						<h3>o	ODBC — ODBC (Unified)</h3>
+						<h3>o	PDO — PHP Data Objects</h3>
+
+						<h1>Way to delimit strings:</h1>
+						<h2>•	Heredoc</h2>
+						<p>-A third way to delimit strings is the heredoc syntax: <<<. After this operator, an identifier is provided, then a newline. The string itself follows, and then the same identifier again to close the quotation. </p>
+						<h2>•	Nowdoc</h2>
+						<p>-Are to single-quoted strings what heredocs are to double-quoted strings. A nowdoc is specified similarly to a heredoc, but no parsing is done inside a nowdoc.</p>
+
+
+							
+
+					</div>
+				</div>
+			
+		  </div>
+				
+		</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
