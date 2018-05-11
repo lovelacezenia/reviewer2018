00cda22b0ddc684f6339ce59a6aa95e8eba867c6
DianneCayabyab <diannecayabyab@gmail.com>
2018-05-11 23:29:49 +0800

    Update servlet.php

--- application/views/pages/servlet.php
+++ application/views/pages/servlet.php
@@ -68,18 +68,6 @@
 		</div>
 	</nav>
 	
-	<div id="colorlib-page">
-		<header>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="colorlib-navbar-brand">
-							<a class="colorlib-logo" href="index.html">Suitcase</a>
-						</div>
-						<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-					</div>
-				</div>
-			</div>
 		</header>
 		<aside id="colorlib-hero">
 			<div class="flexslider">
