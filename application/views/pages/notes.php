79692d754cfdcc257e08c4d09785acc7204b84b5
DianneCayabyab <diannecayabyab@gmail.com>
2018-05-11 23:22:01 +0800

    Update notes.php

--- application/views/pages/notes.php
+++ application/views/pages/notes.php
@@ -8,12 +8,12 @@ if(!isset($this->session->userdata['logged_in'])){
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2 text-center animate-box intro-heading">
-						<h2>WEBTEK FINALS</h2>
+						<h2>WEBTECH FINALS</h2>
 					</div>
 				</div>
 				<div class="row row-padded-bottom">
 					<div class="col-md-6 animate-box text-center">
-						<a href="servlet" class="work-img" style="background-image: url(assets/images/work-1.jpg);">
+						<a href="servlet" class="work-img" style="background-image: url(assets/images/servlets.png);">
 							<div class="overlay"></div>
 							<div class="desc">
 								<span class="icon"><i class="icon-heart-outline"></i></span>
@@ -23,7 +23,7 @@ if(!isset($this->session->userdata['logged_in'])){
 						</a>
 					</div>
 					<div class="col-md-6 animate-box text-center">
-						<a href="sessionhandler" class="work-img" style="background-image: url(assets/images/work-2.jpg);">
+						<a href="sessionhandler" class="work-img" style="background-image: url(assets/images/sh.png);">
 							<div class="overlay"></div>
 							<div class="desc">
 								<span class="icon"><i class="icon-heart-outline"></i></span>
@@ -33,11 +33,21 @@ if(!isset($this->session->userdata['logged_in'])){
 						</a>
 					</div>
 					<div class="col-md-6 animate-box text-center">
-						<a href="ajax" class="work-img" style="background-image: url(assets/images/work-2.jpg);">
+						<a href="ajax" class="work-img" style="background-image: url(assets/images/ajax.png);">
 							<div class="overlay"></div>
 							<div class="desc">
 								<span class="icon"><i class="icon-heart-outline"></i></span>
 								<h3>Ajax</h3>
+								<span class="read-more">Read more <i class="icon-arrow-right3"></i></span>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6 animate-box text-center">
+						<a href="ScopedObjects" class="work-img" style="background-image: url(assets/images/so.png);">
+							<div class="overlay"></div>
+							<div class="desc">
+								<span class="icon"><i class="icon-heart-outline"></i></span>
+								<h3>Scoped Objects</h3>
 								<span class="read-more">Read more <i class="icon-arrow-right3"></i></span>
 							</div>
 						</a>
