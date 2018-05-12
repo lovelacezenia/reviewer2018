f52fe91380ab411559594205c3cc09a7216cd3e9
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-05-12 15:07:01 +0800

    minor fix on UI

--- application/models/User_db.php
+++ application/models/User_db.php
@@ -7,32 +7,33 @@ class User_db extends CI_Model
     {
 
         $this->db->select('*,CONCAT(firstname," ",lastname) AS name')
-            ->where('idno', $data['idno'])
+            ->where('idno like binary', $data['idno'])
             ->where('status', 'active')
             ->limit(1);
         $uname = $this->db->get('user')->row();
 
         $password = $data['password'];
         if ($uname == true) {
-
-            if ($password == $uname->password) {
-                return $uname;
+            if (password_verify($password, $uname->password)) {
+                return true;
             }
         }
-        return false;
+            return false;
     }
 
     //forsignup
     public function signup()
     {
-
+        $password = $this->input->post('password');
+        $options = ['cost' => 12];
+        $hashpassword =  password_hash($password, PASSWORD_DEFAULT, $options);
 
         $data = array(
             'firstname' => $this->input->post('firstname'),
             'lastname' => $this->input->post('lastname'),
             'email' => $this->input->post('email'),
             'idno' => $this->input->post('idno'),
-            'password' => $this->input->post('password'),
+            'password' => $hashpassword,
             'contact_no' => $this->input->post('contact_no'),
             'class_id' => $this->input->post('classcode'),
             'position' => $this->input->post('position')
